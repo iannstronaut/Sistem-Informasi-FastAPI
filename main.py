3c76c1c8325ac8de5793b77ce881d5a1783c8e64
UKM Seni Theatrisic <113005391+iannstronaut@users.noreply.github.com>
2024-10-14 17:44:13 +0700

    feat: middleware

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, HTTPException, status
 from models.migration import RunMigration
-from routers import api_router
+from routers import api_router, public_router
+from services.middleware import Middleware
+
 
 app = FastAPI()
 RunMigration.run()
 
+@app.middleware("http")
+async def auth_middleware(request: Request, call_next):
+    try:
+        # Memanggil auth middleware
+        response = await Middleware.auth(request, call_next)
+        return response
+    except Exception as e:
+        # Menangkap exception dan mengembalikan HTTPException
+        print(f"Error: {e}")  # Log error ke konsol
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=str(e))
+
 app.include_router(api_router.router)
+app.include_router(public_router.router)
